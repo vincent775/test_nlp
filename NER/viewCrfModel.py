6bf5fc9f592564bc4b420d27623f7df86601ee8f
weiyt <weiyt@instrument.com.cn>
2020-03-02 18:49:19 +0800

    如何使用CRF代码

--- NER/viewCrfModel.py
+++ NER/viewCrfModel.py
@@ -30,8 +30,50 @@ def f1(path):
         loc_R = 1.0*correct_loc_tag/loc_tag
         print('loc_P:{0},loc_R:{1},loc_F1:{2}'.format(loc_p,loc_R,(2*loc_p*loc_R)/(loc_p+loc_R)))
 
+def load_model(path):
+    import os,CRFPP
+    if os.path.exists(path):
+        return CRFPP.Tagger('-m {0} -v 3 -n2'.format(path))
+    return None
+
+def locationNER(text):
+    tagger = load_model('./model')
+
+    for c in text:
+        tagger.add(c)
+    result = []
+
+    tagger.parse()
+    word = ''
+    for i  in range(0,tagger.size()):
+        for j in range(0,tagger.xsize()):
+            ch = tagger.x(i,j)
+            tag = tagger.y2(i)
+            if tag =='B':
+                word = ch
+            elif tag == 'M':
+                word += ch
+            elif tag == 'E':
+                word += ch
+                result.append(word)
+            elif tag =='S':
+                word = ch
+                result.append(word)
+    return result
+
+
 if __name__ == '__main__':
     f1('./data/test.rst')
+
+    text ='我中午要去北京饭店'
+    text = '我中午要去北京饭店，下午去中山公园，晚上回亚运村。'
+    print(text, locationNER(text), sep='==> ')
+
+    text = '我去回龙观，不去南锣鼓巷'
+    print(text, locationNER(text), sep='==> ')
+
+    text = '打的去北京南站地铁站'
+    print(text, locationNER(text), sep='==> ')
 
 
 
