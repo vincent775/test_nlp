3d52cc7ec38baa863205c93c0b2ce760a40e22ec
weiyt <weiyt@instrument.com.cn>
2020-05-18 14:52:27 +0800

    分类训练代码

--- classification/classifier.py
+++ classification/classifier.py
@@ -14,7 +14,8 @@ def get_data():
         ham_data = ham_f.readlines()
         spam_data = spam_f.readlines()
         ham_label = np.ones(len(ham_data)).tolist()
-        spam_label = np.ones(len(spam_data)).tolist()
+        ###,因为np.ones为默认值为1的向量   np.zeros为默认值为0的向量
+        spam_label = np.zeros(len(spam_data)).tolist()
         corpus = ham_data+spam_data
         labels = ham_label+spam_label
     return corpus,labels
@@ -41,6 +42,43 @@ def remove_empty_docs(corpus, labels):
 
     return filtered_corpus, filtered_labels
 
+#训练分类的入口方法
+def train_predict_evaluate_model(classifier,
+                                     train_features, train_labels,
+                                     test_features, test_labels):
+
+    # build model
+    classifier.fit(train_features, train_labels)
+    # predict using model
+    predictions = classifier.predict(test_features)
+    # evaluate model prediction performance
+    get_metrics(true_labels=test_labels,
+                predicted_labels=predictions)
+    return predictions
+
+from sklearn import metrics
+#评估
+def get_metrics(true_labels, predicted_labels):
+    print('准确率:', np.round(
+        metrics.accuracy_score(true_labels,
+                               predicted_labels),
+        2))
+    print('精度:', np.round(
+        metrics.precision_score(true_labels,
+                                predicted_labels,
+                                average='weighted'),
+        2))
+    print('召回率:', np.round(
+        metrics.recall_score(true_labels,
+                             predicted_labels,
+                             average='weighted'),
+        2))
+    print('F1得分:', np.round(
+        metrics.f1_score(true_labels,
+                         predicted_labels,
+                         average='weighted'),
+        2))
+
 def main():
     corpus,labels = get_data() #获取数据集
     print('总的数据量：',len(corpus))
@@ -49,7 +87,7 @@ def main():
     print('样本之一：',corpus[0])
     print('样本的label:',labels[243])
     label_name_map = ['垃圾邮件','正常邮件']
-    print('实际：',label_name_map[int(labels[10])],label_name_map[int(labels[59])])
+    print('实际：',label_name_map[int(labels[10])],label_name_map[int(labels[8908])])
     #对数据进行划分
     train_corpus,test_corpus,train_labels,test_labels=prepare_datasets(corpus,labels,test_data_proportion=0.3)
     print('训练数据量：',len(train_corpus))
@@ -57,8 +95,8 @@ def main():
 
     from normalization import normalize_corpus
     #对数据归一化
-    normalize_train_corpus = normalize_corpus(train_corpus)
-    normalize_test_corpus = normalize_corpus(test_corpus)
+    norm_train_corpus = normalize_corpus(train_corpus)
+    norm_test_corpus = normalize_corpus(test_corpus)
 
     ''.strip()
 
@@ -67,10 +105,94 @@ def main():
     import jieba
 
     #词袋模型特征
-    bow_vectorizer,bow_train_features = bow_extractor(normalize_train_corpus)
-    bow_test_features = bow_vectorizer.trainsform(normalize_test_corpus)
+    bow_vectorizer,bow_train_features = bow_extractor(norm_train_corpus)
+    bow_test_features = bow_vectorizer.transform(norm_test_corpus)
 
----------
+    #tfidf 特征
+    tfidf_vectorizer,tfidf_train_features = tfidf_extractor(norm_train_corpus)
+    tfidf_test_features = tfidf_vectorizer.transform(norm_test_corpus)
+
+    # tokenize documents
+    tokenized_train = [jieba.lcut(text) for text  in norm_train_corpus]
+    print(tokenized_train[2:10])
+    tokenized_test = [jieba.lcut(text) for text in norm_test_corpus]
+
+
+   #训练分类器
+    from sklearn.naive_bayes import MultinomialNB
+    from sklearn.linear_model import SGDClassifier
+    from sklearn.linear_model import LogisticRegression
+    mnb = MultinomialNB()
+    # svm = SGDClassifier(loss='hinge', n_iter=100)
+    svm = SGDClassifier(loss='hinge')
+    lr = LogisticRegression()
+
+    #基于词袋模型的多项朴素贝叶斯
+    print('基于词袋模型特征的贝叶斯分类器')
+    mnb_bow_predictions = train_predict_evaluate_model(classifier=mnb,
+                                                       train_features=bow_train_features,
+                                                       train_labels=train_labels,
+                                                       test_features=bow_test_features,
+                                                       test_labels=test_labels)
+    # 基于词袋模型的逻辑回归
+    print("基于词袋模型特征的逻辑回归")
+    lr_bow_predictions = train_predict_evaluate_model(classifier=lr,
+                                                      train_features=bow_train_features,
+                                                      train_labels=train_labels,
+                                                      test_features=bow_test_features,
+                                                      test_labels=test_labels)
+    # 基于词袋模型的支持向量机
+    print('基于词袋模型特征的支持向量机')
+    svm_bow_predictions = train_predict_evaluate_model(classifier=svm,
+                                                       train_features=bow_train_features,
+                                                       train_labels=train_labels,
+                                                       test_features=bow_test_features,
+                                                       test_labels=test_labels)
+
+    # 基于tfidf的多项式朴素贝叶斯模型
+    print('基于tfidf的多项式朴素贝叶斯模型')
+    mnb_tfidf_predictions = train_predict_evaluate_model(classifier=mnb,
+                                                       train_features=tfidf_train_features,
+                                                       train_labels=train_labels,
+                                                       test_features=tfidf_test_features,
+                                                       test_labels=test_labels)
+    # 基于tfidf的逻辑回归模型
+    print('基于tfidf的逻辑回归模型')
+    lr_tfidf_predictions = train_predict_evaluate_model(classifier=lr,
+                                                         train_features=tfidf_train_features,
+                                                         train_labels=train_labels,
+                                                         test_features=tfidf_test_features,
+                                                         test_labels=test_labels)
+    # 基于tfidf的支持向量机模型
+    print('基于tfidf支持向量机模型')
+    svm_tfidf_predictions = train_predict_evaluate_model(classifier=svm,
+                                                         train_features=tfidf_train_features,
+                                                         train_labels=train_labels,
+                                                         test_features=tfidf_test_features,
+                                                         test_labels=test_labels)
+
+
+    #显示部分正确归类和部分错误归类
+    import re
+    num =0
+    for document,label,predicted_label in zip(test_corpus,test_labels,svm_tfidf_predictions):
+        if label==0 and predicted_label ==0:
+            print('邮件类型：',label_name_map[int(label)])
+            print('预测的邮件类型：',label_name_map[int(predicted_label)])
+            print('文本：-')
+            print(re.sub('\n',' ',document))
+            num+=1
+            if num==4:
+                break
+        if label == 1 and predicted_label == 0:
+            print('邮件类型：', label_name_map[int(label)])
+            print('预测的邮件类型：', label_name_map[int(predicted_label)])
+            print('文本：-')
+            print(re.sub('\n', ' ', document))
+            num += 1
+            if num == 4:
+                break
+
 
 
 
--- classification/feature_extractors.py
+++ classification/feature_extractors.py
@@ -2,6 +2,32 @@
 
 #两种模式的特征提取方式
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+'''
+  文本特征提取：
+      将文本数据转化成特征向量的过程
+      比较常用的文本特征表示法为词袋法
+  词袋法：
+      不考虑词语出现的顺序，每个出现过的词汇单独作为一列特征
+      这些不重复的特征词汇集合为词表
+      每一个文本都可以在很长的词表上统计出一个很多列的特征向量
+      如果每个文本都出现的词汇，一般被标记为 停用词 不计入特征向量
+
+  主要有两个api来实现 CountVectorizer 和 TfidfVectorizer
+  CountVectorizer：
+      只考虑词汇在文本中出现的频率
+  TfidfVectorizer：
+      除了考量某词汇在文本出现的频率，还关注包含这个词汇的所有文本的数量
+      能够削减高频没有意义的词汇出现带来的影响, 挖掘更有意义的特征
+
+  相比之下，文本条目越多，Tfid的效果会越显著
+
+
+  下面对两种提取特征的方法，分别设置停用词和不停用，
+  使用朴素贝叶斯进行分类预测，比较评估效果
+
+  '''
+
 
 #基于词袋模型提取特征
 def bow_extractor(corpus,ngram_rang=(1,1)):
@@ -15,10 +41,10 @@ def tfidf_transformer(bow_matrix):
     transformer = TfidfTransformer(norm='12',smooth_idf=True,use_idf=True)
     tfidf_matrix = transformer.fit_transform(bow_matrix)
     return transformer,tfidf_matrix
-from sklearn.feature_extraction.text import TfidfVectorizer
+
 
 def tfidf_extractor(corpus,ngram_range=(1,1)):
-    vectorizer = TfidfVectorizer(min_df=1,norm='12',smooth_idf=True,use_idf=True,ngram_range=ngram_range)
+    vectorizer = TfidfVectorizer(min_df=1,norm='l2',smooth_idf=True,use_idf=True,ngram_range=ngram_range)
     features = vectorizer.fit_transform(corpus)
     return vectorizer,features
 
